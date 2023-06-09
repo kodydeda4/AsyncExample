3e09e4676d004f3d80b724b38a45e002cf14d864
Kody Deda <kodydeda4@gmail.com>
2023-06-09 17:57:22 -0400

    Initial Commit

---
+++ AsyncExample/AsyncExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsyncExampleApp.swift
+//  AsyncExample
+//
+//  Created by Kody Deda on 6/9/23.
+//
+
+import SwiftUI
+
+@main
+struct AsyncExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
